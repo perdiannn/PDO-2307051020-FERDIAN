eb0a528868bd65c55f70b45c0d8b506a12d7e23c
Ferdian <ferdi4nn@gmail.com>
2025-03-12 20:35:58 +0700

    Update FormMahasiswa

---
+++ Form3.Designer.vb
@@ -0,0 +1,314 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form3
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.btngrade = New System.Windows.Forms.Button()
+        Me.tbxuas = New System.Windows.Forms.TextBox()
+        Me.labeluts = New System.Windows.Forms.Label()
+        Me.tbxuts = New System.Windows.Forms.TextBox()
+        Me.tbxtugas = New System.Windows.Forms.TextBox()
+        Me.labeluas = New System.Windows.Forms.Label()
+        Me.labeltugas = New System.Windows.Forms.Label()
+        Me.labelnilai = New System.Windows.Forms.Label()
+        Me.rbtnperempuan = New System.Windows.Forms.RadioButton()
+        Me.rbtnlaki = New System.Windows.Forms.RadioButton()
+        Me.labeljeniskelamin = New System.Windows.Forms.Label()
+        Me.cmbxjurusan = New System.Windows.Forms.ComboBox()
+        Me.cmbxfakultas = New System.Windows.Forms.ComboBox()
+        Me.tbxnama = New System.Windows.Forms.TextBox()
+        Me.tbxnip = New System.Windows.Forms.TextBox()
+        Me.labeljurusan = New System.Windows.Forms.Label()
+        Me.labelfakultas = New System.Windows.Forms.Label()
+        Me.labelnama = New System.Windows.Forms.Label()
+        Me.labelnip = New System.Windows.Forms.Label()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.btnprocess = New System.Windows.Forms.Button()
+        Me.Panel1.SuspendLayout()
+        Me.Panel2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.Controls.Add(Me.btngrade)
+        Me.Panel1.Controls.Add(Me.tbxuas)
+        Me.Panel1.Controls.Add(Me.labeluts)
+        Me.Panel1.Controls.Add(Me.tbxuts)
+        Me.Panel1.Controls.Add(Me.tbxtugas)
+        Me.Panel1.Controls.Add(Me.labeluas)
+        Me.Panel1.Controls.Add(Me.labeltugas)
+        Me.Panel1.Controls.Add(Me.labelnilai)
+        Me.Panel1.Controls.Add(Me.rbtnperempuan)
+        Me.Panel1.Controls.Add(Me.rbtnlaki)
+        Me.Panel1.Controls.Add(Me.labeljeniskelamin)
+        Me.Panel1.Controls.Add(Me.cmbxjurusan)
+        Me.Panel1.Controls.Add(Me.cmbxfakultas)
+        Me.Panel1.Controls.Add(Me.tbxnama)
+        Me.Panel1.Controls.Add(Me.tbxnip)
+        Me.Panel1.Controls.Add(Me.labeljurusan)
+        Me.Panel1.Controls.Add(Me.labelfakultas)
+        Me.Panel1.Controls.Add(Me.labelnama)
+        Me.Panel1.Controls.Add(Me.labelnip)
+        Me.Panel1.Location = New System.Drawing.Point(22, 26)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(1038, 326)
+        Me.Panel1.TabIndex = 0
+        '
+        'btngrade
+        '
+        Me.btngrade.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btngrade.Location = New System.Drawing.Point(896, 261)
+        Me.btngrade.Name = "btngrade"
+        Me.btngrade.Size = New System.Drawing.Size(118, 46)
+        Me.btngrade.TabIndex = 40
+        Me.btngrade.Text = "GRADE"
+        Me.btngrade.UseVisualStyleBackColor = True
+        '
+        'tbxuas
+        '
+        Me.tbxuas.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.tbxuas.Location = New System.Drawing.Point(766, 269)
+        Me.tbxuas.Name = "tbxuas"
+        Me.tbxuas.Size = New System.Drawing.Size(100, 30)
+        Me.tbxuas.TabIndex = 39
+        '
+        'labeluts
+        '
+        Me.labeluts.AutoSize = True
+        Me.labeluts.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.labeluts.Location = New System.Drawing.Point(527, 272)
+        Me.labeluts.Name = "labeluts"
+        Me.labeluts.Size = New System.Drawing.Size(53, 25)
+        Me.labeluts.TabIndex = 35
+        Me.labeluts.Text = "UTS"
+        '
+        'tbxuts
+        '
+        Me.tbxuts.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.tbxuts.Location = New System.Drawing.Point(586, 269)
+        Me.tbxuts.Name = "tbxuts"
+        Me.tbxuts.Size = New System.Drawing.Size(100, 30)
+        Me.tbxuts.TabIndex = 38
+        '
+        'tbxtugas
+        '
+        Me.tbxtugas.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.tbxtugas.Location = New System.Drawing.Point(407, 267)
+        Me.tbxtugas.Name = "tbxtugas"
+        Me.tbxtugas.Size = New System.Drawing.Size(100, 30)
+        Me.tbxtugas.TabIndex = 37
+        '
+        'labeluas
+        '
+        Me.labeluas.AutoSize = True
+        Me.labeluas.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.labeluas.Location = New System.Drawing.Point(706, 272)
+        Me.labeluas.Name = "labeluas"
+        Me.labeluas.Size = New System.Drawing.Size(54, 25)
+        Me.labeluas.TabIndex = 36
+        Me.labeluas.Text = "UAS"
+        '
+        'labeltugas
+        '
+        Me.labeltugas.AutoSize = True
+        Me.labeltugas.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.labeltugas.Location = New System.Drawing.Point(333, 272)
+        Me.labeltugas.Name = "labeltugas"
+        Me.labeltugas.Size = New System.Drawing.Size(68, 25)
+        Me.labeltugas.TabIndex = 34
+        Me.labeltugas.Text = "Tugas"
+        '
+        'labelnilai
+        '
+        Me.labelnilai.AutoSize = True
+        Me.labelnilai.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.labelnilai.Location = New System.Drawing.Point(23, 274)
+        Me.labelnilai.Name = "labelnilai"
+        Me.labelnilai.Size = New System.Drawing.Size(49, 25)
+        Me.labelnilai.TabIndex = 33
+        Me.labelnilai.Text = "Nilai"
+        '
+        'rbtnperempuan
+        '
+        Me.rbtnperempuan.AutoSize = True
+        Me.rbtnperempuan.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.rbtnperempuan.Location = New System.Drawing.Point(529, 114)
+        Me.rbtnperempuan.Name = "rbtnperempuan"
+        Me.rbtnperempuan.Size = New System.Drawing.Size(138, 29)
+        Me.rbtnperempuan.TabIndex = 32
+        Me.rbtnperempuan.TabStop = True
+        Me.rbtnperempuan.Text = "Perempuan"
+        Me.rbtnperempuan.UseVisualStyleBackColor = True
+        '
+        'rbtnlaki
+        '
+        Me.rbtnlaki.AutoSize = True
+        Me.rbtnlaki.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.rbtnlaki.Location = New System.Drawing.Point(338, 114)
+        Me.rbtnlaki.Name = "rbtnlaki"
+        Me.rbtnlaki.Size = New System.Drawing.Size(116, 29)
+        Me.rbtnlaki.TabIndex = 31
+        Me.rbtnlaki.TabStop = True
+        Me.rbtnlaki.Text = "Laki-Laki"
+        Me.rbtnlaki.UseVisualStyleBackColor = True
+        '
+        'labeljeniskelamin
+        '
+        Me.labeljeniskelamin.AutoSize = True
+        Me.labeljeniskelamin.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.labeljeniskelamin.Location = New System.Drawing.Point(23, 114)
+        Me.labeljeniskelamin.Name = "labeljeniskelamin"
+        Me.labeljeniskelamin.Size = New System.Drawing.Size(135, 25)
+        Me.labeljeniskelamin.TabIndex = 30
+        Me.labeljeniskelamin.Text = "Jenis Kelamin"
+        '
+        'cmbxjurusan
+        '
+        Me.cmbxjurusan.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cmbxjurusan.FormattingEnabled = True
+        Me.cmbxjurusan.Location = New System.Drawing.Point(338, 206)
+        Me.cmbxjurusan.Name = "cmbxjurusan"
+        Me.cmbxjurusan.Size = New System.Drawing.Size(345, 33)
+        Me.cmbxjurusan.TabIndex = 29
+        '
+        'cmbxfakultas
+        '
+        Me.cmbxfakultas.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cmbxfakultas.FormattingEnabled = True
+        Me.cmbxfakultas.Location = New System.Drawing.Point(338, 159)
+        Me.cmbxfakultas.Name = "cmbxfakultas"
+        Me.cmbxfakultas.Size = New System.Drawing.Size(281, 33)
+        Me.cmbxfakultas.TabIndex = 28
+        '
+        'tbxnama
+        '
+        Me.tbxnama.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.tbxnama.Location = New System.Drawing.Point(338, 59)
+        Me.tbxnama.Name = "tbxnama"
+        Me.tbxnama.Size = New System.Drawing.Size(465, 30)
+        Me.tbxnama.TabIndex = 27
+        '
+        'tbxnip
+        '
+        Me.tbxnip.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.tbxnip.Location = New System.Drawing.Point(338, 15)
+        Me.tbxnip.Name = "tbxnip"
+        Me.tbxnip.Size = New System.Drawing.Size(242, 30)
+        Me.tbxnip.TabIndex = 26
+        '
+        'labeljurusan
+        '
+        Me.labeljurusan.AutoSize = True
+        Me.labeljurusan.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.labeljurusan.Location = New System.Drawing.Point(23, 214)
+        Me.labeljurusan.Name = "labeljurusan"
+        Me.labeljurusan.Size = New System.Drawing.Size(83, 25)
+        Me.labeljurusan.TabIndex = 24
+        Me.labeljurusan.Text = "Jurusan"
+        '
+        'labelfakultas
+        '
+        Me.labelfakultas.AutoSize = True
+        Me.labelfakultas.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.labelfakultas.Location = New System.Drawing.Point(23, 167)
+        Me.labelfakultas.Name = "labelfakultas"
+        Me.labelfakultas.Size = New System.Drawing.Size(86, 25)
+        Me.labelfakultas.TabIndex = 23
+        Me.labelfakultas.Text = "Fakultas"
+        '
+        'labelnama
+        '
+        Me.labelnama.AutoSize = True
+        Me.labelnama.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.labelnama.Location = New System.Drawing.Point(23, 64)
+        Me.labelnama.Name = "labelnama"
+        Me.labelnama.Size = New System.Drawing.Size(64, 25)
+        Me.labelnama.TabIndex = 22
+        Me.labelnama.Text = "Nama"
+        '
+        'labelnip
+        '
+        Me.labelnip.AutoSize = True
+        Me.labelnip.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.labelnip.Location = New System.Drawing.Point(23, 20)
+        Me.labelnip.Name = "labelnip"
+        Me.labelnip.Size = New System.Drawing.Size(44, 25)
+        Me.labelnip.TabIndex = 21
+        Me.labelnip.Text = "NIP"
+        '
+        'Panel2
+        '
+        Me.Panel2.Controls.Add(Me.btnprocess)
+        Me.Panel2.Location = New System.Drawing.Point(22, 372)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(1038, 55)
+        Me.Panel2.TabIndex = 1
+        '
+        'btnprocess
+        '
+        Me.btnprocess.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnprocess.Location = New System.Drawing.Point(3, 3)
+        Me.btnprocess.Name = "btnprocess"
+        Me.btnprocess.Size = New System.Drawing.Size(133, 49)
+        Me.btnprocess.TabIndex = 25
+        Me.btnprocess.Text = "Process"
+        Me.btnprocess.UseVisualStyleBackColor = True
+        '
+        'Form3
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1080, 449)
+        Me.Controls.Add(Me.Panel2)
+        Me.Controls.Add(Me.Panel1)
+        Me.Name = "Form3"
+        Me.Text = "Form3"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.Panel2.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents Panel1 As System.Windows.Forms.Panel
+    Friend WithEvents Panel2 As System.Windows.Forms.Panel
+    Friend WithEvents rbtnperempuan As System.Windows.Forms.RadioButton
+    Friend WithEvents rbtnlaki As System.Windows.Forms.RadioButton
+    Friend WithEvents labeljeniskelamin As System.Windows.Forms.Label
+    Friend WithEvents cmbxjurusan As System.Windows.Forms.ComboBox
+    Friend WithEvents cmbxfakultas As System.Windows.Forms.ComboBox
+    Friend WithEvents tbxnama As System.Windows.Forms.TextBox
+    Friend WithEvents tbxnip As System.Windows.Forms.TextBox
+    Friend WithEvents btnprocess As System.Windows.Forms.Button
+    Friend WithEvents labeljurusan As System.Windows.Forms.Label
+    Friend WithEvents labelfakultas As System.Windows.Forms.Label
+    Friend WithEvents labelnama As System.Windows.Forms.Label
+    Friend WithEvents labelnip As System.Windows.Forms.Label
+    Friend WithEvents tbxuas As System.Windows.Forms.TextBox
+    Friend WithEvents labeluts As System.Windows.Forms.Label
+    Friend WithEvents tbxuts As System.Windows.Forms.TextBox
+    Friend WithEvents tbxtugas As System.Windows.Forms.TextBox
+    Friend WithEvents labeluas As System.Windows.Forms.Label
+    Friend WithEvents labeltugas As System.Windows.Forms.Label
+    Friend WithEvents labelnilai As System.Windows.Forms.Label
+    Friend WithEvents btngrade As System.Windows.Forms.Button
+End Class
---
+++ Form3.vb
@@ -0,0 +1,174 @@
+﻿Public Class Form3
+
+    Private Sub Form3_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        cmbxfakultas.Items.Add("FMIPA")
+        cmbxfakultas.Items.Add("FKIP")
+        cmbxfakultas.Items.Add("FEB")
+
+        If cmbxfakultas.Items.Count > 0 Then
+            cmbxfakultas.SelectedIndex = 0
+        End If
+    End Sub
+
+    Private Sub Panel1_Paint(sender As Object, e As PaintEventArgs) Handles Panel1.Paint
+
+    End Sub
+
+    Private Sub btnprocess_Click(sender As Object, e As EventArgs) Handles btnprocess.Click
+        Dim nip As String = tbxnip.Text
+        Dim nama As String = tbxnama.Text
+        Dim fakultas As String = cmbxfakultas.Text
+        Dim jurusan As String = cmbxjurusan.Text
+        Dim jeniskelamin As String = ""
+        If rbtnlaki.Checked Then
+            jeniskelamin = "Laki-Laki"
+        ElseIf rbtnperempuan.Checked Then
+            jeniskelamin = "Perempuan"
+        End If
+
+        Dim message As String = "Hai : " & nip & vbCrLf &
+                                "Nip : " & nama & vbCrLf &
+                                "Fakultas : " & fakultas & vbCrLf &
+                                "Jurusan : " & jurusan & vbCrLf &
+                                "Jenis Kelamin : " & jeniskelamin
+
+        MessageBox.Show(message, "Informasi", MessageBoxButtons.OK, MessageBoxIcon.Information)
+    End Sub
+
+    Private Sub labelnip_Click(sender As Object, e As EventArgs) Handles labelnip.Click
+
+    End Sub
+
+    Private Sub tbxnip_TextChanged(sender As Object, e As EventArgs) Handles tbxnip.TextChanged
+
+    End Sub
+
+    Private Sub labelnama_Click(sender As Object, e As EventArgs) Handles labelnama.Click
+
+    End Sub
+
+    Private Sub tbxnama_TextChanged(sender As Object, e As EventArgs) Handles tbxnama.TextChanged
+
+    End Sub
+
+    Private Sub labeljeniskelamin_Click(sender As Object, e As EventArgs) Handles labeljeniskelamin.Click
+
+    End Sub
+
+    Private Sub rbtnlaki_CheckedChanged(sender As Object, e As EventArgs) Handles rbtnlaki.CheckedChanged
+
+    End Sub
+
+    Private Sub rbtnperempuan_CheckedChanged(sender As Object, e As EventArgs) Handles rbtnperempuan.CheckedChanged
+
+    End Sub
+
+    Private Sub labelfakultas_Click(sender As Object, e As EventArgs) Handles labelfakultas.Click
+
+    End Sub
+
+    Private Sub cmbxfakultas_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cmbxfakultas.SelectedIndexChanged
+        cmbxjurusan.Items.Clear()
+
+        Dim fakultas As String = cmbxfakultas.Text
+
+        Select Case fakultas
+            Case "FMIPA"
+                cmbxjurusan.Items.Add("Matematika")
+                cmbxjurusan.Items.Add("Kimia")
+                cmbxjurusan.Items.Add("Biologi")
+                cmbxjurusan.Items.Add("Fisika")
+                cmbxjurusan.Items.Add("Ilmu Komputer")
+
+            Case "FKIP"
+                cmbxjurusan.Items.Add("Pendidikan Matematika")
+                cmbxjurusan.Items.Add("Pendidikan Kimia")
+                cmbxjurusan.Items.Add("Pendidikan Biologi")
+                cmbxjurusan.Items.Add("Pendidikan Fisika")
+                cmbxjurusan.Items.Add("Pendidikan Bahasa Indonesia")
+
+            Case "FEB"
+                cmbxjurusan.Items.Add("Manajemen")
+                cmbxjurusan.Items.Add("Akuntansi")
+                cmbxjurusan.Items.Add("Bisnis Digital")
+                cmbxjurusan.Items.Add("Pemasaran")
+                cmbxjurusan.Items.Add("Keuangan dan Perbankan")
+        End Select
+
+        If cmbxjurusan.Items.Count > 0 Then
+            cmbxjurusan.SelectedIndex = 0
+        End If
+    End Sub
+
+    Private Sub labeljurusan_Click(sender As Object, e As EventArgs) Handles labeljurusan.Click
+
+    End Sub
+
+    Private Sub cmbxjurusan_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cmbxjurusan.SelectedIndexChanged
+
+    End Sub
+
+    Private Sub labelnilai_Click(sender As Object, e As EventArgs) Handles labelnilai.Click
+
+    End Sub
+
+    Private Sub labeltugas_Click(sender As Object, e As EventArgs) Handles labeltugas.Click
+
+    End Sub
+
+    Private Sub tbxtugas_TextChanged(sender As Object, e As EventArgs) Handles tbxtugas.TextChanged
+
+    End Sub
+
+    Private Sub labeluts_Click(sender As Object, e As EventArgs) Handles labeluts.Click
+
+    End Sub
+
+    Private Sub tbxuts_TextChanged(sender As Object, e As EventArgs) Handles tbxuts.TextChanged
+
+    End Sub
+
+    Private Sub labeluas_Click(sender As Object, e As EventArgs) Handles labeluas.Click
+
+    End Sub
+
+    Private Sub tbxuas_TextChanged(sender As Object, e As EventArgs) Handles tbxuas.TextChanged
+
+    End Sub
+
+    Private Sub btngrade_Click(sender As Object, e As EventArgs) Handles btngrade.Click
+        Dim tugas As Double = Val(tbxtugas.Text)
+        Dim uts As Double = Val(tbxuts.Text)
+        Dim uas As Double = Val(tbxuas.Text)
+
+        If tugas < 0 Or tugas > 100 Or uts < 0 Or uts > 100 Or uas < 0 Or uas > 100 Then
+            MessageBox.Show("Nilai harus antara 0 - 100", "Peringatan", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            Exit Sub
+        End If
+
+        ' Hitung nilai akhir berdasarkan bobot
+        Dim nilaiAkhir As Double = (tugas * 0.3) + (uts * 0.3) + (uas * 0.4)
+
+        ' Tentukan grade
+        Dim grade As String
+        If nilaiAkhir >= 78 And nilaiAkhir <= 90 Then
+            grade = "A"
+        ElseIf nilaiAkhir >= 65 And nilaiAkhir < 78 Then
+            grade = "B"
+        ElseIf nilaiAkhir >= 50 And nilaiAkhir < 65 Then
+            grade = "C"
+        ElseIf nilaiAkhir >= 40 And nilaiAkhir < 50 Then
+            grade = "D"
+        Else
+            grade = "E"
+        End If
+
+        MessageBox.Show("Nilai Akhir: " & nilaiAkhir.ToString("0.00") & vbCrLf &
+                        "Grade: " & grade,
+                        "Hasil", MessageBoxButtons.OK, MessageBoxIcon.Information)
+    End Sub
+
+    Private Sub Panel2_Paint(sender As Object, e As PaintEventArgs) Handles Panel2.Paint
+
+    End Sub
+End Class
